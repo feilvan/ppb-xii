eafae1a74195d81c83ff629358fd934fda9af806
Fuad Elhasan Irfani <feilvan@feilvan.com>
2026-09-17 08:50:04 +0700

    260917 asyncstorage update

--- app/async.tsx
+++ app/async.tsx
@@ -3,21 +3,22 @@ import { useState } from "react";
 import { Button, Text, View } from "react-native";
 
 export default function Async() {
-  const [nama, setNama] = useState({});
+  const [nama, setNama] = useState("");
   const simpanNama = async () => {
-    await AsyncStorage.setItem("nama", "Adi");
+    await AsyncStorage.setItem("nama", "AAAA");
   };
 
   const ambilNama = async () => {
-    const data = await AsyncStorage.getItem("nama");
-    setNama(data ? data : "");
+    await AsyncStorage.getItem("nama", (error, result) => {
+      setNama(result ? result : "");
+    });
   };
 
   return (
     <View>
       <Button title="Simpan" onPress={simpanNama} />
       <Button title="Ambil" onPress={ambilNama} />
-      <Text>{String(nama)}</Text>
+      <Text>{nama}</Text>
     </View>
   );
 }
